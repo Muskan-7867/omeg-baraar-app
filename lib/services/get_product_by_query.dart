427d49024b481f57339bfc957a81f5f29b80daed
Muskan-7867 <muskanloach984@gmail.com>
2025-06-23 13:26:18 +0530

    filtered prod api

--- lib/services/get_product_by_query.dart
+++ lib/services/get_product_by_query.dart
@@ -1,19 +1,23 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class ProductService {
-  static const String baseUrl = "https://omeg-bazaar-backend.onrender.com/api/v1"; 
+class GetFilteredProducts {
+  static const String baseUrl = "https://omeg-bazaar-backend.onrender.com/api/v1";
 
-  Future<List<Map<String, dynamic>>> fetchProducts({
+  Future<List<Map<String, dynamic>>> fetchFilteredProducts({
     int limit = 10,
     int page = 1,
     String? category,
     String? search,
+    double minPrice = 0,
+    double maxPrice = 1000000000000,
   }) async {
     final uri = Uri.parse('$baseUrl/product/getquery').replace(
       queryParameters: {
         'limit': limit.toString(),
         'page': page.toString(),
+        'minPrice': minPrice.toString(),
+        'maxPrice': maxPrice.toString(),
         if (category != null && category != 'all') 'category': category,
         if (search != null && search.trim().isNotEmpty) 'search': search,
       },
