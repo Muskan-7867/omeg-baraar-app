e1fb5361fa5b432d41fddf9497938ec17e17c676
Muskan-7867 <muskanloach984@gmail.com>
2025-06-26 12:23:01 +0530

    show account icon on the basis of user is loggined or not

--- lib/widgets/common/app_bar.dart
+++ lib/widgets/common/app_bar.dart
@@ -3,20 +3,48 @@ import 'package:omeg_bazaar/provider/cart_provider.dart';
 import 'package:omeg_bazaar/screens/cart/cartpage.dart';
 import 'package:omeg_bazaar/utills/app_colour.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class OmAppBar extends StatelessWidget implements PreferredSizeWidget {
+class OmAppBar extends StatefulWidget implements PreferredSizeWidget {
   final String title;
 
   const OmAppBar({super.key, required this.title});
+
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  State<OmAppBar> createState() => _OmAppBarState();
+}
+
+class _OmAppBarState extends State<OmAppBar> {
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isLoggedIn = prefs.getString('authToken') != null;
+    if (mounted && _isLoggedIn != isLoggedIn) {
+      setState(() {
+        _isLoggedIn = isLoggedIn;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<CartProvider>(
       builder: (context, cart, child) {
         return AppBar(
-          title: Text(title, style: const TextStyle(color: Colors.white)),
+          title: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
           backgroundColor: AppColour.primaryColor,
           actions: [
             Padding(
@@ -29,7 +57,7 @@ class OmAppBar extends StatelessWidget implements PreferredSizeWidget {
                         children: [
                           IconButton(
                             onPressed: () {
-                             cart.loadCartCount();
+                              cart.loadCartCount();
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
@@ -64,18 +92,30 @@ class OmAppBar extends StatelessWidget implements PreferredSizeWidget {
                             ),
                         ],
                       ),
-
                       IconButton(
                         onPressed: () {
-                          // Add your login navigation logic here
-                          Navigator.pushNamed(context, '/login');
+                          if (_isLoggedIn) {
+                            Navigator.pushNamed(context, '/profile');
+                          } else {
+                            Navigator.pushNamed(
+                              context,
+                              '/login',
+                            ).then((_) => _checkLoginStatus());
+                          }
                         },
-                        icon: Image.asset(
-                          "assets/icons/login.png",
-                          color: Colors.white,
-                          width: 35,
-                          height: 35,
-                        ),
+                        icon:
+                            _isLoggedIn
+                                ? const Icon(
+                                  Icons.account_circle,
+                                  color: Colors.white,
+                                  size: 32,
+                                )
+                                : Image.asset(
+                                  "assets/icons/login.png",
+                                  color: Colors.white,
+                                  width: 35,
+                                  height: 35,
+                                ),
                       ),
                     ],
                   ),
