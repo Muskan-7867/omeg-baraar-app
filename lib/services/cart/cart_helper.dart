473382114156e255f895059c834d7505410a4956
Muskan-7867 <muskanloach984@gmail.com>
2025-06-30 15:59:11 +0530

    cart services

---
+++ lib/services/cart/cart_helper.dart
@@ -0,0 +1,34 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartHelper {
+  static Future<List<String>> getCartIds() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList('cartProdIds') ?? [];
+  }
+
+  static Future<bool> isInCart(String id) async {
+    final ids = await getCartIds();
+    return ids.contains(id);
+  }
+
+  static Future<void> addToCart(String id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final ids = prefs.getStringList('cartProdIds') ?? [];
+    if (!ids.contains(id)) {
+      ids.add(id);
+      await prefs.setStringList('cartProdIds', ids);
+    }
+  }
+
+  static Future<void> removeFromCart(String id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final ids = prefs.getStringList('cartProdIds') ?? [];
+    ids.remove(id);
+    await prefs.setStringList('cartProdIds', ids);
+  }
+
+  static Future<List<String>> getCartItems() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList('cart') ?? [];
+  }
+}
---
+++ lib/services/cart/get_cart_prod_by_id.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:omeg_bazaar/utills/api.constraints.dart';
+
+//http://10.0.2.2:3000
+class GetCartProdById {
+ static const baseUrl = ApiConstants.baseUrl;
+
+  Future<List> get(List<String> ids) async {
+    final uri = Uri.parse("$baseUrl/product/cartproducts");
+
+    final response = await http.post(
+      uri,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"ids": ids}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final products = data['products'];
+
+      if (products is List) {
+        return products;
+      } else {
+        throw Exception("Unexpected format: 'products' is not a List");
+      }
+    } else {
+      throw Exception("Failed to load cart products: ${response.statusCode}");
+    }
+  }
+}
