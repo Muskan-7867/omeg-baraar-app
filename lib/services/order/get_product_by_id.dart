06fb10f505814f1c3798adbd74b2f916e4a1fb2b
Muskan-7867 <muskanloach984@gmail.com>
2025-07-02 13:56:03 +0530

    add new order apis

--- lib/services/order/get_product_by_id.dart
+++ lib/services/order/get_product_by_id.dart
@@ -4,16 +4,16 @@ import 'package:http/http.dart' as http;
 import 'package:omeg_bazaar/utills/api.constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class OrderService {
+class ProductService {
   static const baseUrl = ApiConstants.baseUrl;
 
-  static Future<Map<String, dynamic>> getOrderDetails(String orderId) async {
+  static Future<Map<String, dynamic>> getProductById(String singleproductid) async {
     try {
       final prefs = await SharedPreferences.getInstance();
       final token = prefs.getString('authToken');
 
       final response = await http.get(
-        Uri.parse("$baseUrl/order/single/$orderId"),
+        Uri.parse("$baseUrl/product/single/$singleproductid"),
         headers: {
           'Authorization': 'Bearer $token',
           'Content-Type': 'application/json',
@@ -23,10 +23,10 @@ class OrderService {
       if (response.statusCode == 200) {
         return json.decode(response.body);
       } else {
-        throw Exception('Failed to load order details');
+        throw Exception('Failed to load product details');
       }
     } catch (e) {
-      throw Exception('Error fetching order details: $e');
+      throw Exception('Error fetching product details: $e');
     }
   }
 }
