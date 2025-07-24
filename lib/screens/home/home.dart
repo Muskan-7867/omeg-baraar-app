3c526dd0f2ef6c9d9fb86f050b1a3833397c4941
Muskan-7867 <muskanloach984@gmail.com>
2025-07-24 12:22:19 +0530

    FIX

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -31,7 +31,7 @@ class _HomeState extends State<Home> {
     ];
 
     return Scaffold(
-      appBar: _currentIndex == 2 ? null : OmAppBar(title: 'Omeg-Bazaar'),
+      appBar: _currentIndex == 2 ? null : OmAppBar(title: 'OMEG-BAZAAR'),
       body: pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
