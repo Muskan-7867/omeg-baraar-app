45b9ebbaaa678509c04d3690c9c6c11fae92354f
Muskan-7867 <muskanloach984@gmail.com>
2025-09-17 11:33:38 +0530

    update home page

--- lib/screens/home/widgets/banner.dart
+++ lib/screens/home/widgets/banner.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:omeg_bazaar/utills/app_colour.dart';
 
@@ -10,42 +11,108 @@ class MyBanner extends StatefulWidget {
 }
 
 class _MyBannerState extends State<MyBanner> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  final List<String> _banners = [
+    "assets/images/banner1.jpg",
+    "assets/images/banner2.jpg",
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    // Auto-slide every 3 seconds
+    Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      if (_currentPage < _banners.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 400),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
+      height: 220,
       width: double.infinity,
       decoration: BoxDecoration(
-        color: AppColour.primaryColor,
         borderRadius: BorderRadius.circular(20),
+        color: AppColour.primaryColor,
       ),
-
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Stack(
+          fit: StackFit.expand,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Start Shopping!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
+            /// PageView for multiple banners
+            PageView.builder(
+              controller: _pageController,
+              itemCount: _banners.length,
+              itemBuilder: (context, index) {
+                return Image.asset(_banners[index], fit: BoxFit.cover);
+              },
+            ),
+
+            /// Text on top of banner
+            Positioned(
+              top: 16,
+              left: 16,
+              right: 16,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Start Shopping!",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(height: 1),
+                  Text(
+                    "With Omeg Bazaar!!",
+                    style: TextStyle(color: Colors.grey[700], fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+
+            /// Page indicator (dots)
+            Positioned(
+              bottom: 10,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  _banners.length,
+                  (index) => AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    height: 8,
+                    width: _currentPage == index ? 16 : 8,
+                    decoration: BoxDecoration(
+                      color:
+                          _currentPage == index ? Colors.white : Colors.white54,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   ),
                 ),
-
-                Text(
-                  'With Omeg Bazaar!!',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-              ],
+              ),
             ),
-            Image.asset("assets/images/bag.png"),
           ],
         ),
       ),
